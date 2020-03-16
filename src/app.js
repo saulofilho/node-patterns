5c5a25cfca6a0bda36c16d6ccfb0eb824a12c895
Saulo Filho <32524964+saulofilho@users.noreply.github.com>
2020-03-16 20:21:58 -0300

    security

--- src/app.js
+++ src/app.js
@@ -1,13 +1,20 @@
 import 'dotenv/config';
+
 import express from 'express';
 import path from 'path';
 import cors from 'cors';
-import * as Sentry from '@sentry/node';
+import helmet from 'helmet';
+import redis from 'redis';
+import RateLimit from 'express-rate-limit';
+import RateLimitRedis from 'rate-limit-redis';
 import Youch from 'youch';
+import * as Sentry from '@sentry/node';
 import 'express-async-errors';
+
 import routes from './routes';
-import './database';
 import sentryConfig from './config/sentry';
+
+import './database';
 
 class App {
   constructor() {
@@ -22,12 +29,33 @@ class App {
 
   middlewares() {
     this.server.use(Sentry.Handlers.requestHandler());
-    this.server.use(cors());
+    this.server.use(helmet());
+    this.server.use(
+      cors({
+        origin: false,
+      })
+    );
     this.server.use(express.json());
+
     this.server.use(
       '/files',
       express.static(path.resolve(__dirname, '..', 'tmp', 'uploads'))
     );
+
+    if (process.env.NODE_ENV !== 'development') {
+      this.server.use(
+        new RateLimit({
+          store: new RateLimitRedis({
+            client: redis.createClient({
+              host: process.env.REDIS_HOST,
+              port: process.env.REDIS_PORT,
+            }),
+          }),
+          windowMs: 1000 * 60 * 15,
+          max: 10,
+        })
+      );
+    }
   }
 
   routes() {
